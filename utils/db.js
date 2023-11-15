8927ec833f112cf63daa2b37b951de133ed3c454
TJonCanon <taylor.joyner93@me.com>
2023-11-15 17:23:39 -0600

    task 1 maybe

---
+++ utils/db.js
@@ -0,0 +1,34 @@
+// mongodb intialization
+const { MongoClient } = require('mongodb');
+
+//  mongodb class
+class DBClient {
+    constructor() {
+      const host = process.env.DB_HOST || 'localhost';
+      const port = process.env.DB_PORT || 27017;
+      const database = process.env.DB_DATABASE || 'files_manager';
+      this.url = `mongodb://${host}:${port}/${database}`;
+      this.client = new MongoClient(this.url, { useUnifiedTopology: true });
+      this.client.connect();
+    }
+  
+    // checks the active db connection
+    isAlive() {
+      return this.client.isConnected();
+    }
+  
+    // returns number of docs in collection users
+    async nbUsers() {
+      const db = this.client.db();
+      const collection = db.collection('users');
+      return collection.countDocuments();
+    }
+  
+    // returns number of docs in collection files
+    async nbFiles() {
+      const db = this.client.db();
+      const collection = db.collection('files');
+      return collection.countDocuments();
+    }
+  }
+  
