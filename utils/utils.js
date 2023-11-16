55c5146f8c03d406156665f5bb3420eda8590bb8
WoodsonTD <taylordwoodson@gmail.com>
2023-11-16 10:59:46 -0600

    add

---
+++ utils/utils.js
@@ -0,0 +1,27 @@
+
+const checkRedisStatus = async () => {
+  // Implement logic to check Redis status
+  // Return true if Redis is alive, false otherwise
+};
+
+const checkDBStatus = async () => {
+  // Implement logic to check DB status
+  // Return true if DB is alive, false otherwise
+};
+
+const countUsers = async () => {
+  // Implement logic to count users in the database
+  // Return the count
+};
+
+const countFiles = async () => {
+  // Implement logic to count files in the database
+  // Return the count
+};
+
+module.exports = {
+  checkRedisStatus,
+  checkDBStatus,
+  countUsers,
+  countFiles,
+};
