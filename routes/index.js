dacdbf38fe9113e3d09d258374793757e8829c42
WoodsonTD <taylordwoodson@gmail.com>
2023-11-20 13:54:10 -0600

    task 5

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
+import FilesContoller from '../controllers/FilesContoller';
 
 const router = Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 router.post('/users', UsersController.postNew);
+router.post('/files', FilesController.postUpload);
 
 export default router;
