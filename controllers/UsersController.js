770ae25e76e1d4e56b2ea4916f70a2ff09746832
linszay <lindseyjlancaster@gmail.com>
2023-11-20 00:02:41 -0600

    files for task 3

---
+++ controllers/UsersController.js
@@ -0,0 +1,47 @@
+// controllers/UsersController.js
+const { hash } = require('crypto');
+const UserModel = require('../models/UserModel');
+
+const UsersController = {
+  postNew: async (req, res) => {
+    const { email, password } = req.body;
+
+    // check if email and password args are valid
+    if (!email) {
+      return res.status(400).json({ error: 'Missing email' });
+    }
+
+    if (!password) {
+      return res.status(400).json({ error: 'Missing password' });
+    }
+
+    try {
+      // check if the email already exists in db
+      const existingUser = await UserModel.findOne({ email });
+
+      if (existingUser) {
+        return res.status(400).json({ error: 'Already exist' });
+      }
+
+      // hashing the password using SHA1
+      const hashedPassword = hash('sha1').update(password).digest('hex');
+
+      // create new user
+      const newUser = new UserModel({
+        email,
+        password: hashedPassword,
+      });
+
+      // save user to db
+      await newUser.save();
+
+      // return new user with only email and id
+      return res.status(201).json({ id: newUser._id, email: newUser.email });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+  },
+};
+
+export default UsersController;
