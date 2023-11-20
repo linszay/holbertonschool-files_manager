9b38707c08917650b0f0eb0e060a0632463fe63f
WoodsonTD <taylordwoodson@gmail.com>
2023-11-20 15:09:38 -0600

    add

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -3,24 +3,18 @@ import fs from 'fs/promises';
 import path from 'path';
 import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
+import mimeTypes from 'mime-types';
+import { createQueue, addJob } from '../utils/queue';
+import imageThumbnail from 'image-thumbnail';
 
+const fileQueue = createQueue('fileQueue');
 const FOLDER_PATH = process.env.FOLDER_PATH || '/tmp/files_manager';
 
 const FilesController = {
   async postUpload(req, res) {
     try {
-      const { 'x-token': token } = req.headers;
-
-      if (!token) {
-        return res.status(401).json({ error: 'Unauthorized' });
-      }
-
-      const userId = await redisClient.get(`auth_${token}`);
-      if (!userId) {
-        return res.status(401).json({ error: 'Unauthorized' });
-      }
-
-      const { name, type, parentId = 0, isPublic = false, data } = req.body;
+      const { name, type, data, parentId, isPublic } = req.body;
+      const { userId } = req;
 
       if (!name) {
         return res.status(400).json({ error: 'Missing name' });
@@ -30,11 +24,13 @@ const FilesController = {
         return res.status(400).json({ error: 'Missing type' });
       }
 
-      if ((type === 'file' || type === 'image') && !data) {
+      if (!data && type !== 'folder') {
         return res.status(400).json({ error: 'Missing data' });
       }
 
-      if (parentId !== 0) {
+      let localPath;
+
+      if (parentId) {
         const parentFile = await dbClient.filesCollection.findOne({
           _id: dbClient.getObjectId(parentId),
         });
@@ -48,31 +44,99 @@ const FilesController = {
         }
       }
 
-      const fileData = {
+      if (type === 'folder') {
+        const newFile = await dbClient.filesCollection.insertOne({
+          userId: dbClient.getObjectId(userId),
+          name,
+          type,
+          isPublic: isPublic || false,
+          parentId: parentId || 0,
+        });
+
+        return res.status(201).json(newFile.ops[0]);
+      } else {
+        localPath = path.join(FOLDER_PATH, uuidv4());
+
+        await fs.writeFile(localPath, Buffer.from(data, 'base64'));
+      }
+
+      const newFile = await dbClient.filesCollection.insertOne({
         userId: dbClient.getObjectId(userId),
         name,
         type,
-        isPublic,
-        parentId: dbClient.getObjectId(parentId),
-      };
+        isPublic: isPublic || false,
+        parentId: parentId || 0,
+        localPath,
+      });
 
-      if (type === 'file' || type === 'image') {
-        const fileUuid = uuidv4();
-        const localPath = path.join(FOLDER_PATH, fileUuid);
-        await fs.writeFile(localPath, Buffer.from(data, 'base64'));
+      // Add a job to the queue for generating thumbnails
+      addJob(fileQueue, { userId, fileId: newFile.ops[0]._id });
 
-        fileData.localPath = localPath;
-      }
-
-      const result = await dbClient.filesCollection.insertOne(fileData);
-      const newFile = { id: result.insertedId.toString(), ...fileData };
-
-      return res.status(201).json(newFile);
+      return res.status(201).json(newFile.ops[0]);
     } catch (error) {
       console.error(`Error in postUpload: ${error}`);
       return res.status(500).json({ error: 'Internal Server Error' });
     }
   },
+
+  async getFile(req, res) {
+    try {
+      const { id } = req.params;
+      const { userId } = req;
+
+      const file = await dbClient.filesCollection.findOne({
+        _id: dbClient.getObjectId(id),
+      });
+
+      if (!file || (file.isPublic === false && file.userId.toString() !== userId)) {
+        return res.status(404).json({ error: 'Not found' });
+      }
+
+      const { localPath, name, type } = file;
+
+      // Check if the size query parameter is present
+      const size = req.query.size;
+      if (size) {
+        const thumbnailPath = path.join(FOLDER_PATH, `${id}_${size}`);
+        try {
+          await fs.access(thumbnailPath);
+          const thumbnailContent = await fs.readFile(thumbnailPath);
+          res.status(200).send(thumbnailContent);
+          return;
+        } catch (error) {
+          // Thumbnail not found, continue to fetch the original file
+        }
+      }
+
+      // Get the MIME-type based on the name of the file
+      const mimeType = mimeTypes.lookup(name) || 'application/octet-stream';
+
+      // Return the content of the file with the correct MIME-type
+      const fileContent = await fs.readFile(localPath, { encoding: 'utf-8' });
+      res.setHeader('Content-Type', mimeType);
+      res.status(200).send(fileContent);
+    } catch (error) {
+      console.error(`Error in getFile: ${error}`);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+  },
+
+  async getIndex(req, res) {
+    // ... (implementation for GET /files)
+  },
+
+  async getShow(req, res) {
+    // ... (implementation for GET /files/:id)
+  },
+
+  async putPublish(req, res) {
+    // ... (implementation for PUT /files/:id/publish)
+  },
+
+  async putUnpublish(req, res) {
+    // ... (implementation for PUT /files/:id/unpublish)
+  },
 };
 
 export default FilesController;
+
---
+++ controllers/worker.js
@@ -0,0 +1,51 @@
+import { createWorker } from 'bull';
+import dbClient from './utils/db';
+import imageThumbnail from 'image-thumbnail';
+import path from 'path';
+import fs from 'fs/promises';
+
+const fileQueue = createWorker('fileQueue');
+
+fileQueue.on('failed', (job, err) => {
+  console.error(`Job ${job.id} failed with error: ${err.message}`);
+});
+
+fileQueue.process(async (job) => {
+  try {
+    const { userId, fileId } = job.data;
+
+    // Validate job data
+    if (!userId || !fileId) {
+      throw new Error('Missing userId or fileId');
+    }
+
+    // Check if the file document exists in DB
+    const file = await dbClient.filesCollection.findOne({
+      _id: dbClient.getObjectId(fileId),
+      userId: dbClient.getObjectId(userId),
+    });
+
+    if (!file) {
+      throw new Error('File not found');
+    }
+
+    // Generate thumbnails
+    const originalPath = file.localPath || path.join(FOLDER_PATH, uuidv4());
+    const sizes = [500, 250, 100];
+
+    for (const size of sizes) {
+      const thumbnailPath = path.join(FOLDER_PATH, `${fileId}_${size}`);
+      await imageThumbnail({
+        source: originalPath,
+        destination: thumbnailPath,
+        width: size,
+        responseType: 'base64',
+      });
+    }
+  } catch (error) {
+    console.error(`Error in fileQueue process: ${error}`);
+    throw error;
+  }
+});
+
+export default fileQueue;
