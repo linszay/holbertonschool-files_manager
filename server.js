1d244949267eac0a1eb205bd923a56c1f61ba11f
WoodsonTD <taylordwoodson@gmail.com>
2023-11-20 11:34:02 -0600

    add

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-const express = require('express');
-const routes = require('./routes');
+import express from 'express';
+import routes from './routes';
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
-app.use('/', routes);
+app.use(express.json());
+app.use(routes);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
