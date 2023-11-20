4758565f879cf1766de81cdf11683f8829deaff1
WoodsonTD <taylordwoodson@gmail.com>
2023-11-20 14:22:13 -0600

    task5

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
-import FilesContoller from '../controllers/FilesContoller';
+import FilesController from '../controllers/FilesController';
 
 const router = Router();
 
