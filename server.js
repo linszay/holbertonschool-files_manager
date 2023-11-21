8a6e99f3f2a3de8a8a381ba0e5a0a8d7494e36d3
linszay <lindseyjlancaster@gmail.com>
2023-11-21 15:31:16 -0600

    updated server.js for linter

--- server.js
+++ server.js
@@ -7,9 +7,8 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(routes);
 
-const PORT = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`Server is running on port 5000`);
+  console.log(`Server is running on port ${port}`);
 });
 
-module.exports = app;
+export default app;
