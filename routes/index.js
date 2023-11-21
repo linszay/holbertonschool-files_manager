aecbe1f69e8d4e798ba96fa11026fdba56f0de95
TJonCanon <taylor.joyner93@me.com>
2023-11-21 15:18:34 -0600

    reverted index

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,19 @@
-// routing for our api
-const express = require('express');
-const AppController = require('../controllers/AppController');
-const UsersController = require('../controllers/UsersController');
-const AuthController = require('../controllers/AuthController');
-const FilesController = require('../controllers/FilesController');
+const express = require('express')
+import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
 
-// declare our router
 const router = express.Router();
 
 // get routes
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+router.post('/users', UsersController.postNew);
+router.get('/users/me', UsersController.getMe);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
-router.get('/users/me', UsersController.getMe);
-router.get('/files/:id', FilesController.getShow);
-router.get('/files', FilesController.getIndex);
-router.get('/files/:id/data', FilesController.getFile);
-
-// post routes
-router.post('/users', UsersController.postNew);
-router.post('/files', FilesController.postUpload);
-
-// put routes
-router.put('/files/:id/publish', FilesController.putPublish);
-router.put('/files/:id/unpublish', FilesController.putUnpublish);
+router.post('/files', FilesController.postUpload); // Updated route
 
 module.exports = router;
+
