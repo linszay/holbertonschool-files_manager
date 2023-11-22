2b82ea9468d1a381458a6854622cd2e5a0ccc153
linszay <lindseyjlancaster@gmail.com>
2023-11-21 20:04:19 -0600

    please be the last time please

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -2,7 +2,6 @@
 const sha1 = require('sha1');
 const redisClient = require('../utils/redis');
 const dbClient = require('../utils/db');
-
 const UsersController = {
   postNew: async (req, res) => {
     const { email, password } = req.body;
@@ -13,7 +12,6 @@ const UsersController = {
     if (!password) {
       return res.status(400).json({ error: 'Missing password' });
     }
-
     try {
       // check if email already exists in db
       const existingUser = await dbClient
@@ -24,23 +22,19 @@ const UsersController = {
       if (existingUser) {
         return res.status(400).json({ error: 'Already exist' });
       }
-
       // hashing the password using SHA1
       const hashedPassword = sha1(password);
-
       // create new user
       const newUser = {
         email,
         password: hashedPassword,
       };
-
       // save user to db
       const result = await dbClient
         .client
         .db()
         .collection('users')
         .insertOne(newUser);
-
       // return new user with only email and id
       return res.status(201).json({ id: result.insertedId, email: newUser.email });
     } catch (error) {
@@ -50,27 +44,26 @@ const UsersController = {
   },
   getMe: async (req, res) => {
     const { 'x-token': token } = req.headers;
-
     if (!token) {
       return res.status(401).json({ error: 'Unauthorized' });
     }
-
     try {
       // retrieve user based on token
       const userId = await redisClient.get(`auth_${token}`);
-
       if (!userId) {
         return res.status(401).json({ error: 'Unauthorized' });
       }
-
       // retrieve user details from MongoDB
       const user = await dbClient
         .client
         .db()
         .collection('users')
         .findOne({ _id: userId });
-
       if (!user) {
+        return res.status(401).json({ error: 'Unauthorized' });
+      }
+
+      if (user._id.toString() !== userId) {
         return res.status(401).json({ error: 'Unauthorized' });
       }
 
@@ -82,5 +75,3 @@ const UsersController = {
     }
   },
 };
-
-module.exports = UsersController;
